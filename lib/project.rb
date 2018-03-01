5220a9ef2469907b36f19fc6a68c85660df74f7e
Alyssa Kim <alyssa.kim@gmail.com>
2018-03-01 22:49:35 +0000

    automatically backed up by learn

--- lib/project.rb
+++ lib/project.rb
@@ -5,10 +5,11 @@ class Project
   
   def initialize(title)
     @title = title
+    @backers = []
     #binding.pry
   end 
   
-  def add_backer
-    
+  def add_backer(backer)
+    @backers << backer 
   end 
 end 
