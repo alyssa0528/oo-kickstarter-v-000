abc9a3915ee566024f09bbc73d25fef768e3f949
Alyssa Kim <alyssa.kim@gmail.com>
2018-03-01 22:44:28 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -3,11 +3,10 @@ require 'pry'
 class Backer
   attr_accessor :name
   attr_reader :title
-  
-  @backed_projects = []
 
   def initialize(name)
     @name = name
+    @backed_projects = []
   end 
   
   #argument is a project object; 
