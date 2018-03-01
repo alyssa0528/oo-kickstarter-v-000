bf980826831758b92d0c4664e6b42eab16429e26
Alyssa Kim <alyssa.kim@gmail.com>
2018-03-01 23:13:44 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -12,7 +12,8 @@ class Backer
   #argument is a project object; 
   def back_project(project)
     @backed_projects << project
-    project.backers << self.name 
+    project.backers << self.name
+    binding.pry
   end
   
   def backed_projects
